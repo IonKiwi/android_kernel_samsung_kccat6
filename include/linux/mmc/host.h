9750c61f9d3e9108126c62c1501d875c021dbf66
Mohan Srinivasan <srmohan@google.com>
2016-08-04 15:17:29 -0700

    Android: MMC IO Latency Histograms.
    This patch adds a new sysfs node (latency_hist) and reports IO
    (svc time) latency histograms. Disabled by default, can be enabled
    by echoing 0 into latency_hist, stats can be cleared by writing
    2 into latency_hist.
    
    Change-Id: I26c77a63af39417812e3989c836b4b5b684dfa85
    Signed-off-by: Mohan Srinivasan <srmohan@google.com>
    Bug: 30677035

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -227,6 +227,35 @@ enum dev_state {
 	DEV_RESUMED,
 };
 
+/*
+ * X-axis for IO latency histogram support.
+ */
+static const u_int64_t latency_x_axis_us[] = {
+	100,
+	200,
+	300,
+	400,
+	500,
+	600,
+	700,
+	800,
+	900,
+	1000,
+	1200,
+	1400,
+	1600,
+	1800,
+	2000,
+	2500,
+	3000,
+	4000,
+	5000,
+	6000,
+	7000,
+	9000,
+	10000
+};
+
 struct mmc_host {
 	struct device		*parent;
 	struct device		class_dev;
@@ -460,6 +489,16 @@ struct mmc_host {
 	atomic_t	log_count;
 	bool		mmc_core_cmd_logging;
 #endif
+
+#define MMC_IO_LAT_HIST_DISABLE         0
+#define MMC_IO_LAT_HIST_ENABLE          1
+#define MMC_IO_LAT_HIST_ZERO            2
+	int		latency_hist_enabled;
+	u_int64_t	latency_y_axis_read[ARRAY_SIZE(latency_x_axis_us) + 1];
+	u_int64_t	latency_reads_elems;
+	u_int64_t	latency_y_axis_write[ARRAY_SIZE(latency_x_axis_us) + 1];
+	u_int64_t	latency_writes_elems;
+
 	unsigned long		private[0] ____cacheline_aligned;
 };
 
